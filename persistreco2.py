451fd449980c88d5573dcbfcc34a787b478e5e47
Andreas Weiden <andreas.weiden@cern.ch>
2016-05-20 12:14:35 +0200

    Whitespace

--- persistreco2.py
+++ persistreco2.py
@@ -1,20 +1,16 @@
-from Configurables import  DaVinci, DecayTreeTuple, TurboConf
+from Configurables import DaVinci, DecayTreeTuple, TurboConf
 from DecayTreeTuple import Configuration
-
-from Configurables import CombineParticles, DaVinci, FilterDesktop
+from Configurables import CombineParticles
 
 from PhysSelPython.Wrappers import Selection, SelectionSequence, DataOnDemand
 
-TurboConf().PersistReco=True
-
+TurboConf().PersistReco = True
 
 pions = DataOnDemand('Phys/StdAllNoPIDsPions/Particles')
-
 ks0 = CombineParticles('Ks0Topipi',
                         DecayDescriptors=['[KS0 -> pi+ pi+]cc'],
-                        CombinationCut=("AM < 320*MeV"), #parent 
-                        MotherCut="ALL") 
-
+                        CombinationCut=("AM < 320*MeV"), #parent
+                        MotherCut="ALL")
 ks0_sel = Selection(
     'Sel_Ks0Topipi',
     Algorithm=ks0,
@@ -27,22 +23,15 @@ ks0_selseq = SelectionSequence(
 
 
 dtt_ks0 = DecayTreeTuple('TupleKs0Topipi')
-
 dtt_ks0.Inputs = ks0_selseq.outputLocations()
-
 dtt_ks0.Decay = '[KS0 -> pi+ pi+]CC'
-
 dtt_ks0.addBranches({
     'Ks0': '[KS0 -> pi+ pi+]CC',
     'pi1': '[KS0 -> ^pi+ pi+]CC',
-    'pi2': '[KS0 -> pi+ ^pi+]CC'  
+    'pi2': '[KS0 -> pi+ ^pi+]CC'
 })
 
 DaVinci().UserAlgorithms = [dtt_ks0, ks0_selseq.sequence()]
-DaVinci().DataType ='2016'
-
-DaVinci().EvtMax=1000
-
+DaVinci().DataType = '2016'
+DaVinci().EvtMax = 1000
 DaVinci().TupleFile = 'PersistRecoTuple_ks0_pipi.root'
-
-
